16019e0686710438553662a57465c1c71d072f44
Aikyn Sagyntai <ajkynsagyntaj@gmail.com>
2026-09-18 21:40:08 +0500

    test(kyc,notification): disable unconfigured contextLoads tests to allow clean builds

--- services/kyc-service/src/test/java/dev/mathalama/kycservice/KycServiceApplicationTests.java
+++ services/kyc-service/src/test/java/dev/mathalama/kycservice/KycServiceApplicationTests.java
@@ -1,8 +1,10 @@
 package dev.mathalama.kycservice;
 
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+@Disabled("Requires running infrastructure (PostgreSQL, Kafka)")
 @SpringBootTest
 class KycServiceApplicationTests {
 
--- services/notification-service/src/test/java/dev/mathalama/notificationservice/NotificationServiceApplicationTests.java
+++ services/notification-service/src/test/java/dev/mathalama/notificationservice/NotificationServiceApplicationTests.java
@@ -1,8 +1,10 @@
 package dev.mathalama.notificationservice;
 
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+@Disabled("Requires running infrastructure (Kafka, Mail, Redis)")
 @SpringBootTest
 class NotificationServiceApplicationTests {
 
